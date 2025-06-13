8a6f7090c3d6e5da5faefe0c9dd4cf1c7bb157fa
Nehz <arturboyun@gmail.com>
2025-06-13 11:21:51 +0000

    feat(tests): add temperature endpoint test

--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -1,7 +1,11 @@
+from datetime import datetime
+
 import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
 from starlette import status
+
+from weather_api.settings import settings
 
 
 @pytest.mark.anyio
@@ -15,3 +19,21 @@ async def test_health(client: AsyncClient, fastapi_app: FastAPI) -> None:
     url = fastapi_app.url_path_for("health_check")
     response = await client.get(url)
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.anyio
+async def test_temperatures(client: AsyncClient, fastapi_app: FastAPI) -> None:
+    """
+    Checks the temperature endpoint.
+
+    :param client: client for the app.
+    :param fastapi_app: current FastAPI application.
+    """
+    url = fastapi_app.url_path_for("get_temperature")
+    response = await client.get(
+        url,
+        params={"date": datetime.now().date()},
+        headers={"X-Token": settings.x_token},
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert isinstance(response.json(), list)
